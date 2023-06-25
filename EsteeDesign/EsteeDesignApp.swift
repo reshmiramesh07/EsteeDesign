5fef117c52a239dbf538335a2cf6e578318485e5
reshmiramesh07 <reshmiramesh07@gmail.com>
2023-06-24 21:45:03 -0700

    Initial Commit

---
+++ EsteeDesign/EsteeDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeDesignApp.swift
+//  EsteeDesign
+//
+//  Created by Reshmi Ramesh on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
