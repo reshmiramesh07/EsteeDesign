a0c1855d91390231ea2cf851ff86233c30e68a51
reshmiramesh07 <reshmiramesh07@gmail.com>
2023-06-26 05:46:36 -0700

    Built BrowseProducts base, added images

---
+++ EsteeDesign/BrowseProducts.swift
@@ -0,0 +1,71 @@
+//
+//  BrowseProducts.swift
+//  EsteeDesign
+//
+//  Created by Reshmi Ramesh on 6/26/23.
+//
+
+import SwiftUI
+
+struct BrowseProducts: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            HStack {
+                VStack {
+                    Image("pfp-twoinone")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    Text("2-in-1 Foam Cleanser")
+                        .multilineTextAlignment(.center)
+                    Text("Purify, Pores, Glow")
+                        .font(.caption)
+                        .multilineTextAlignment(.center)
+                }
+                VStack {
+                    Image("pfp-airy")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    Text("Airy Lotion")
+                        .multilineTextAlignment(.center)
+                    Text("Pores, Hydration, Glow")
+                        .font(.caption)
+                        .multilineTextAlignment(.center)
+                }
+            }
+            Spacer()
+            
+            HStack {
+                VStack {
+                    Image("pfp-melt")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    Text("Melting Soft Creme / Mask")
+                        .multilineTextAlignment(.center)
+                    Text("Pores, Hydration, Glow")
+                        .font(.caption)
+                        .multilineTextAlignment(.center)
+                }
+                VStack {
+                    Image("pfp-radiant")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    Text("Radiant Essence Lotion")
+                        .multilineTextAlignment(.center)
+                    Text("Prep, Infuse, Glow")
+                        .font(.caption)
+                        .multilineTextAlignment(.center)
+                }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct BrowseProducts_Previews: PreviewProvider {
+    static var previews: some View {
+        BrowseProducts()
+    }
+}
--- EsteeDesign/ContentView.swift
+++ EsteeDesign/ContentView.swift
@@ -10,13 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("test")
+            Text("Hello World")
         }
-        .padding()
     }
 }
 
